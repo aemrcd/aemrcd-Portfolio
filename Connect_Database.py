47ced1e4bddbf59adadead8fc4075795ac013c0f
unknown <Aerolmercado25@gmail.com>
2025-04-11 15:32:37 +0200

    FIX THE DATABASE CONNECTION ISSUE

--- Connect_Database.py
+++ Connect_Database.py
@@ -1,29 +1,26 @@
-from mysql.connector import Error
 import mysql.connector
+from mysql.connector import Error
 
 def connect_to_database():
     try:
-        # XAMMPP MySQL connection
         connection = mysql.connector.connect(
-            host="localhost",  
-            user="root",  
-            password="",  
-            port=3306
+            # host="localhost",
+            # user="root",
+            # password="",
+            # port=3306,
 
-        # UBUNTU MySQL connection
+            host="10.2.3.127",
+            user="AEROL",
+            database="myContact",
+            password="1234"
+
         )
-
         if connection.is_connected():
-            print("Connection to MySQL database was successful!")
-            # You can add additional code here to interact with the database
-
+            print("✅ Connection successful!")
+            return connection
     except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+        print(f"❌ Error: {e}")
+        return None
 
-    finally:
-        if 'connection' in locals() and connection.is_connected():
-            connection.close()
-            print("MySQL connection is closed.")
-
-# Call the function to test the connection
 connect_to_database()
+
